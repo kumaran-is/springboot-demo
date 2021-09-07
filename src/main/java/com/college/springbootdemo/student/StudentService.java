19807af6001cbad5a72883eaa19fcf2de6229a16
kumaran Iyyasamy srinivasan <kumaran.x.iyyasamy.srinivasan.-nd@disney.com>
2021-09-07 16:12:29 -0400

    cleanup

--- src/main/java/com/college/springbootdemo/student/StudentService.java
+++ src/main/java/com/college/springbootdemo/student/StudentService.java
@@ -21,7 +21,6 @@ public class StudentService {
 
 	public List<Student> getStudents() {
 		
-		//return List.of(new Student(10L,"Peter1", "peter@gmail.com", LocalDate.of(2002, 8, 26), 20));
 		return studentRepository.findAll();
 	}
 	
