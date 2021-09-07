f0a60733adbd8dcf9b220156088d02803ce43204
kumaran Iyyasamy srinivasan <kumaran.x.iyyasamy.srinivasan.-nd@disney.com>
2021-09-07 14:48:39 -0400

    added DAO layer and repository to interact with database

--- src/main/java/com/college/springbootdemo/student/StudentService.java
+++ src/main/java/com/college/springbootdemo/student/StudentService.java
@@ -2,13 +2,23 @@ package com.college.springbootdemo.student;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+	
+	private final StudentRepository studentRepository;
+	
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
 
 	public List<Student> getStudents() {
 		
-		return List.of(new Student(10L,"Peter1", "peter@gmail.com", LocalDate.of(2002, 8, 26), 20));
+		//return List.of(new Student(10L,"Peter1", "peter@gmail.com", LocalDate.of(2002, 8, 26), 20));
+		return studentRepository.findAll();
 	}
 }
