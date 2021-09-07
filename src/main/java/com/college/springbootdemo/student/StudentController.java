b3633a2266c0c74a8edaa13611e51bd3804e846f
Kumaran Srinivasan <kumaran.isk@gmail.com>
2021-09-07 16:15:04 -0400

    Implemented CRUD operations using Spring Data JPA and Postgresql   (#1)
    * added service layer
    
    * added DAO layer and repository to interact with database
    
    * implemented crud operations
    
    * cleanup
    
    Co-authored-by: kumaran Iyyasamy srinivasan <kumaran.x.iyyasamy.srinivasan.-nd@disney.com>

--- src/main/java/com/college/springbootdemo/student/StudentController.java
+++ src/main/java/com/college/springbootdemo/student/StudentController.java
@@ -1,20 +1,57 @@
 package com.college.springbootdemo.student;
 
-import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/student")
 public class StudentController {
 	
+
+	private final StudentService studentService;
+	
+	@Autowired
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+		
+	}
+	
 	@GetMapping("/list")
 	public List<Student> getStudents() {
 		
-		return List.of(new Student(10L,"Peter", "peter@gmail.com", LocalDate.of(2002, 8, 26), 20));
+		return studentService.getStudents();
+	}
+	
+	@PostMapping
+	public void addNewStudent(@RequestBody Student student) {
+		
+		studentService.addNewStudent(student);
+	}
+	
+
+	@DeleteMapping("/{id}")
+	public void deleteStudent(@PathVariable Long id) {
+		
+		studentService.deleteStudent(id);
+	}
+	
+	@PutMapping("/{id}")
+	public void modifyStudent(
+			@PathVariable Long id,
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) String email) {
+		
+		studentService.updateStudent(id, name, email);
 	}
 
 }
